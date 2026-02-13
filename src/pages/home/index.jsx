78eb47542e3d3cc90b4019c819c4edd8d5d7b811
gvnrxd <208095437+gvnxr@users.noreply.github.com>
2026-02-13 11:12:50 -0500

    Added league GIFs to Yes and No options

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -4,6 +4,8 @@ import { useState } from "react";
 
 // Images
 import Gif from "@img/default-ask.gif";
+import YesGif from "@img/leona-yes.gif";
+import NoGif from "@img/shrug-jinx.gif";
 
 export default function HomePage() {
   const [step, setStep] = useState(1);
@@ -11,14 +13,14 @@ export default function HomePage() {
 
   const handleNoClick = () => {
     // Grow Yes button
-    setStep((prev) => Math.min(prev + 0.5, 3.5));
+    setStep((prev) => Math.min(prev + 0.25, 1.75));
 
     // Change No text
     setNoIndex((prev) => Math.min(prev + 1, noTexts.length - 1));
   };
 
   const noTexts = [
-    "No 🙅‍♀️",
+    "No ",
     "Are you sure?",
     "Really sure?",
     "Please?",
@@ -32,20 +34,26 @@ export default function HomePage() {
       <div className={styles.ctnr}>
         <h1>Stormy, Will you be my Valentine?</h1>
         <img src={Gif} alt="Valentine GIF" />
-        <div className={`${styles.btnCtnr} ${step >= 2 ? styles.column : ""}`}>
+        <div
+          className={`${styles.btnCtnr} ${step >= 1.5 ? styles.column : ""}`}
+        >
           <Link
             to="/valentine-confirmation"
             style={{ fontSize: `${step}rem` }}
             className={styles.yes}
           >
-            Yes💞
+            <span>
+              Yes <img src={YesGif} alt="Valentine GIF" />
+            </span>
           </Link>
           <button
             style={{ fontSize: `${step}rem` }}
             className={styles.no}
             onClick={handleNoClick}
           >
-            {noTexts[noIndex]}
+            <span>
+              {noTexts[noIndex]} <img src={NoGif} alt="Valentine GIF" />
+            </span>
           </button>
         </div>
       </div>
